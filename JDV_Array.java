9c5cb5f1c7efd396eb0ba77d6acae40c3036a67e
HashKer15 <95391134+HashKer15@users.noreply.github.com>
2024-01-17 17:01:17 +0800

    Update JDV_Array.java

--- JDV_Array.java
+++ JDV_Array.java
@@ -10,6 +10,7 @@ package jdv_array;
  * @author user23
  */
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class JDV_Array {
 
@@ -18,16 +19,21 @@ public class JDV_Array {
      */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Which item do you want to display(0-3): ");
-        int num = input.nextInt();
-        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        int i;
-            for (i = num; i < cars.length; i++) {
-            System.out.println("You Choose " + cars[i]);
-            break;
-        
+
+        System.out.print("Enter how many animals you want to insert? ");
+        int numOfAnimals = input.nextInt();
+
+        String[] animals = new String[numOfAnimals];
+
+        for (int i = 0; i < numOfAnimals; i++) {
+            System.out.print("[" + (i + 1) + "] ");
+            animals[i] = input.next();
+        }
+        Arrays.sort(animals);
+        System.out.println("You entered the following animals:");
+        for (int i = 0; i < animals.length; i++) {
+            System.out.println(animals[i]);
         }
 
-        
     }
 }
